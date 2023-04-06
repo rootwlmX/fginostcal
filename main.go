17e8c5e007cc7314fcaecc36b6bd91e8c0c6f9a7
Arcueid <1170895958@qq.com>
2023-04-06 17:53:09 +0800

    feat: extract sql

--- main.go
+++ main.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"fginostcal/engine"
 	"fginostcal/router"
 	"fginostcal/util"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 )
 
 func main() {
 
-	config, err := util.ParseConfig("./config/test.json")
-	if err != nil {
-		log.Fatal(err.Error())
-		return
-	}
+	/*	config, err := util.ParseConfig("./config/test.json")
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}*/
 
-	_, err = engine.NewOrmEngine(config)
-	if err != nil {
-		log.Fatal(err.Error())
-		return
-	}
+	/*	_, err = engine.NewOrmEngine(config)
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}*/
 
 	if err := util.Init("2021-12-03", 1); err != nil {
 		fmt.Println("Init() failed, err = ", err)
@@ -36,7 +34,7 @@ func main() {
 		c.HTML(200, "servants.html", "flysnow_org")
 	})
 
-	err = r.Run()
+	err := r.Run()
 	if err != nil {
 		return
 	}
